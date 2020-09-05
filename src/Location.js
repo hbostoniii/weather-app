33f251d4ef5bdde8b5c7ceaee313e4d9ff2c6e67
Horace Boston <hbostoniii@gmail.com>
2020-09-04 22:34:55 -0500

    first commit, set up components, layout, and initial styles

---
+++ src/Location.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Location = () => {
+	return (
+		<div className="location">
+			<h1>Timezone</h1>
+			<p>icon</p>
+		</div>
+	);
+};
+
+export default Location;
---
+++ src/Temperature.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Temperature = () => {
+	return (
+		<div className="temperature">
+			<h2>
+				32<span>&#176;F</span>
+			</h2>
+			<div>Cold</div>
+		</div>
+	);
+};
+
+export default Temperature;
